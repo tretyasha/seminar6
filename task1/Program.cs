d8a740178f7ae910b6cd8d1ff495c1a869607432
tretyasha <72045043+tretyasha@users.noreply.github.com>
2023-03-06 17:39:52 +0300

    задача1 решена

---
+++ task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+int[,] arr = new int[6, 5];
+
+Random randomNumber = new Random();
+
+void FillArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = randomNumber.Next(1, 100);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray();
+PrintArray();
