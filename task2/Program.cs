5a715acee83605cb28797b9100e39ad138fd383c
tretyasha <72045043+tretyasha@users.noreply.github.com>
2023-03-06 17:45:13 +0300

    задача2 решена

---
+++ task2/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задать двумерный массив следующим правилом: Aₘₙ = m+n
+var arr = new int[3, 3];
+
+//Random randomDouble = new Random();
+
+void FillArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray();
+PrintArray();
